afd25eee1d9de548972ea98f01c3f0b778b8e9ed
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-12-17 03:51:05 -0300

    Add input search

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,34 @@
+import {useState} from "react";
 import Layout from "../components/layout/Layout.tsx";
+import style from "./Home.module.scss";
+import iconSearch from "../assets/search.svg";
 
 export default function Home() {
+
+    const [input, setInput] = useState("")
+
     return (
         <Layout>
-            <h1>Hello</h1>
+            <div className={style.container}>
+
+                {/* Search input */}
+                <div className={style.input}>
+                    <input
+                        placeholder='Search you pokemon' type="text" value={input}
+                    />
+                    <button>
+                        <img src={iconSearch} alt='search icon' width={30} height={30}/>
+                    </button>
+                </div>
+
+                {/* Container pokemons */}
+                <section className={styles.pokemonsContainer}>
+
+                </section>
+
+            </div>
+            <aside>
+            </aside>
         </Layout>
     )
 }
