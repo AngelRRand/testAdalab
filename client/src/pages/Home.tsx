a84acec429b1b0ae5fe7eb446fc437535e0cb3bd
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2023-12-17 04:20:49 -0300

    Add new styles and responsive

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -56,6 +56,7 @@ export default function Home() {
                                     img={d.imageUrl}
                                     types={d.types}
                                     id={d.id}
+                                    key={d.id}
                                 />
                             )
                         })
